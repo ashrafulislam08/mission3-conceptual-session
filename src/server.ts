455c5e47438533f7920cf05b31b72fba8d1765f7
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-07-05 17:20:40 +0600

    config file

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,20 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import config from "./config";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000`);
+app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
 });
 
 async function server() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(config.mongodb_uri as string);
     console.log("Database is connected!");
   } catch (error) {
     console.log(error);
