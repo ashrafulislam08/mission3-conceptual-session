8a4ad8d8774ec06a8f78931d79c20180f7c5d51b
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-07-05 17:37:09 +0600

    user interface done

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: "I am here.",
+  });
+});
 
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
